0e9e14b1b02d6da2786751ac891d050807f2c4a7
Arthur Pitzer <arthurpitzer@id.uff.br>
2019-05-27 23:10:53 -0300

    auth backend tested

--- micro_tcg/models.py
+++ micro_tcg/models.py
@@ -1,6 +1,5 @@
 import bcrypt
 
-
 def encrypt(value):
     encoded = str(value).encode()
     salt = bcrypt.gensalt()
@@ -20,18 +19,29 @@ class Entity:
         for attr in self.__class__.__attributes__:
             value = get_value_or_default(kwargs, attr)
             setattr(self, attr, value)
+        if '_id' in kwargs:
+            value = kwargs['_id']
+            setattr(self, '_id', value)
 
     @classmethod
     def get_collection(cls, db):
         return db[cls.__collection_name__]
 
+    @classmethod
+    async def get_by_id(cls, db, obj_id):
+        collection = cls.get_collection(db)
+        query = dict(_id=obj_id)
+        obj_data = await collection.find_one(query)
+        return cls(**obj_data)
+
     def as_document(self):
-        return self.__dict__
+        return self.__dict__.copy()
 
     async def save(self, db):
         collection = self.__class__.get_collection(db)
         doc = self.as_document()
         result = await collection.insert_one(doc)
+        setattr(self, '_id', result.inserted_id)
         return result.inserted_id
 
 
@@ -49,7 +59,11 @@ class User(Entity):
         super().__init__(self, *args, **kwargs)
         self._password = None
         if 'password' in kwargs:
-            self.password = kwargs['password']
+            value = kwargs['password']
+            if type(value) is bytes:
+                self._password = value
+            else:
+                self.password = value
 
     @property
     def password(self):
@@ -63,6 +77,13 @@ class User(Entity):
     def password(self):
         del self._password
 
+    def as_document(self):
+        doc = super(User, self).as_document()
+        del doc['_password']
+        doc['password'] = self.password
+        return doc
+
+
     @classmethod
     async def auth_or_none(cls, db, username, password):
         collection = cls.get_collection(db)
@@ -74,7 +95,8 @@ class User(Entity):
         if user.check_password(password):
             user.token = encrypt(user_data['_id'])
             query = dict(_id=user_data['_id'])
-            await collection.update_one(query, user)
+            update_data = {'$set': user.as_document()}
+            await collection.update_one(query, update_data)
             return user
         return None
 
--- micro_tcg/tests/models_user_test.py
+++ micro_tcg/tests/models_user_test.py
@@ -1,10 +1,17 @@
 import unittest
 
 from micro_tcg.models import User
+from micro_tcg.tests.mock_db import create_test_db
+import asyncio
+import json
 
-
-password = 'password123',
+username = 'tester123'
+password = 'password123'
 wrong_password = 'password1234'
+
+
+def sync(f):
+    return asyncio.get_event_loop().run_until_complete(f)
 
 
 class ModelsUserTest(unittest.TestCase):
@@ -22,3 +29,15 @@ class ModelsUserTest(unittest.TestCase):
         assert user.password != password
         assert user.check_password(password)
         assert not user.check_password(wrong_password)
+
+    def test_auth(self):
+        user = User(
+            username=username,
+            password=password
+        )
+        db = sync(create_test_db())
+        sync(user.save(db))
+        auth_user = sync(User.auth_or_none(db, username, password))
+        assert auth_user is not None
+        assert auth_user._id == user._id
+        assert auth_user.token is not None
