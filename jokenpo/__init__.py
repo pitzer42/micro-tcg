233f1af6734b74ebd4344858cc3179fb962d945a
Arthur Pitzer <arthurpitzer@id.uff.br>
2019-06-21 00:02:53 -0300

    jokenpo

---
+++ jokenpo/__init__.py
@@ -0,0 +1,55 @@
+import asyncio
+
+from engine.io.client_connection import ClientConnection
+from engine.io.connection_group import ConnectionGroup
+
+from engine.server import (
+    create_aiohttp_app,
+    run_aiohttp_app
+)
+
+rules = {
+    'p': 'r',
+    'r': 's',
+    's': 'p'
+}
+
+header = 'choose your move \n p - paper \n r - rock \n s - scissors'
+
+
+async def jokenpo_loop(player: ClientConnection, all_players: ConnectionGroup):
+    # first player`s loop does the job. All others can wait.
+    game_over = asyncio.Event()
+    if all_players.clients[0] != player:
+        return await game_over.wait()
+
+    for a in all_players:
+        for b in all_players:
+            if a != b:
+
+                move_a = ''
+                while move_a not in rules.keys():
+                    await a.send(header)
+                    move_a = await a.receive()
+                    print(move_a)
+                    move_a = move_a['message']
+
+                move_b = ''
+                while move_b not in rules.keys():
+                    await b.send(header)
+                    move_b = await b.receive()
+                    print(move_b)
+                    move_b = move_b['message']
+
+                if rules[move_a] == move_b:
+                    await a.send('you won by playing '+move_a+' against '+move_b)
+                    await b.send('you lost by playing ' + move_b + ' against ' + move_a)
+                else:
+                    await a.send('you lost by playing ' + move_a + ' against ' + move_b)
+                    await b.send('you won by playing ' + move_b + ' against ' + move_a)
+    game_over.set()
+
+
+if __name__ == '__main__':
+    app = create_aiohttp_app(game_loop=jokenpo_loop)
+    run_aiohttp_app(app)
---
+++ tests/jokenpo/mock_client.py
@@ -0,0 +1,111 @@
+import asyncio
+
+from aiohttp import ClientSession
+
+from engine import routes
+from engine.models.user import User
+
+from tests.engine_tests.unit.storage.user_repo import user_data
+
+
+class MicroTCGClient:
+
+    def __init__(self, session: ClientSession):
+        self.user = User(**user_data)
+        self.base_url = ''
+        self.session = session
+        self.opponent = None
+        self.socket = None
+
+    async def setup(self):
+        await self.register_user()
+        await self.login()
+        await self.enter_waiting_list()
+
+    async def get_all_users(self):
+        url = self.base_url + routes.users
+        return await self.session.get(url)
+
+    async def register_user(self):
+        url = self.base_url + routes.users
+        doc = self.user.__dict__
+        return await self.session.put(
+            url,
+            json=doc
+        )
+
+    async def login(self, expect_success=True):
+        url = self.base_url + routes.login
+        doc = self.user.__dict__
+        response = await self.session.get(
+            url,
+            json=doc
+        )
+        if expect_success:
+            response_json = await response.json()
+            self.user.token = response_json[User.__token_attr__]
+        return response
+
+    async def login_with_wrong_name(self):
+        bkp = self.user.name
+        self.user.name += 'wrong'
+        response = await self.login(expect_success=False)
+        self.user.name = bkp
+        return response
+
+    async def login_with_wrong_password(self):
+        bkp = self.user.password
+        self.user.password += 'wrong'
+        response = await self.login(expect_success=False)
+        self.user.password = bkp
+        return response
+
+    async def enter_waiting_list(self):
+        url = self.base_url + routes.waiting_list
+        self.socket = await self.session.ws_connect(url)
+        doc = dict(
+            token=self.user.token
+        )
+        await self.socket.send_json(doc)
+        return await self.receive_and_print()
+
+    async def receive_and_send_loop(self):
+        while True:
+            await self.receive_and_print()
+            await asyncio.sleep(0.5)
+            await self.prompt_and_send()
+
+    async def prompt_and_send(self):
+        message = input(':')
+        package = dict(
+            message=message
+        )
+        await self.socket.send_json(package)
+        return message
+
+    async def receive_and_print(self):
+        answer = await self.socket.receive_json()
+        text = self.user.name + ' < ' + repr(answer)
+        print(text)
+        return answer
+
+
+if __name__ == '__main__':
+
+    async def client_cli():
+        print('Micro-TCG')
+
+        client = MicroTCGClient(
+            ClientSession()
+        )
+
+        client.base_url = 'http://localhost:8080'
+
+        name = input('name:')
+        if name != '':
+            client.user.name = name
+
+        await client.setup()
+        await client.receive_and_send_loop()
+
+    asyncio.run(client_cli())
